a78ce0311fa403ef4f960bc99f923080df54f8d5
Mehmet Gezer <112784374+Mehmetica@users.noreply.github.com>
2025-01-16 20:12:03 +0100

    Update Kitap_transactions.py

--- Kitap_transactions.py
+++ Kitap_transactions.py
@@ -1,7 +1,77 @@
-def add_book():
-    pass
+import json
+from datetime import datetime, timedelta
+import Zaman
 
-def delete_book():
-    pass
 
-#.......
+file_name="kitap.json"
+
+def read_json_file(file_name):
+    try:
+        with open(file_name, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+def write_json_file(file_name, data):
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+def add_book(file_name, book):
+    books = read_json_file(file_name)
+    books.append(book)
+    write_json_file(file_name, books)
+    print("Kitap başarıyla eklendi.")
+
+def get_book_info():
+    Barkod = int(input("Barkod numarasını giriniz: "))
+    Dil = input("Kitabın dilini giriniz: ")
+    Fiyat = float(input("Kitabın fiyatını giriniz: "))
+    Kitap_Adi = input("Kitabın adını giriniz: ")
+    Yayinevi = input("Kitabın yayınevini giriniz: ")
+    Yazar = input("Kitabın yazarını giriniz: ")
+    eklenme_tarihi = Zaman.eklenme_tarihi()
+    iade_tarihi = Zaman.iade_tarihi()
+    
+
+    new_book = {
+        "Barkod": Barkod,
+        "Dil": Dil,
+        "Fiyat": Fiyat,
+        "Kitap_Adi": Kitap_Adi,
+        "Yayinevi": Yayinevi,
+        "Yazar": Yazar,
+        "Eklenme_Tarihi": eklenme_tarihi,
+        "Iade_Tarihi": iade_tarihi
+    }
+    return new_book
+
+def delete_book(file_name,barkod):
+    while True:
+        try:
+            books = read_json_file(file_name)
+            books = [book for book in books if book["Barkod"] != barkod]
+            write_json_file(file_name, books)
+            print("Kitap başarıyla silindi.")
+            break
+        except Exception:
+            print("Hatali giris yaptiniz, tekrar deneyiniz.")
+
+
+def list_books(file_name):
+    books = read_json_file(file_name)
+    for book in books:
+        print(book)
+
+
+def search_book(file_name, keyword):
+    books = read_json_file(file_name)
+    found_books = [book for book in books if keyword.lower() in book["Kitap_Adi"].lower()]
+    if found_books:
+        for book in found_books:
+            print(f"{book} isimli kitap kutuphanemizde mevcuttur.")
+    else:
+        print("Kitap bulunamadı.")
+
+
+
